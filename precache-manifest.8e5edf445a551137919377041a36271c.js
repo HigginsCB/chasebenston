4f3ce5ea701a77fff0865714e3039d9fd627aa92
Chase Benston <cbenston@ius.edu>
2020-10-23 06:55:55 -0400

    Updates

---
+++ precache-manifest.8e5edf445a551137919377041a36271c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d0e72a9c2be743b6196449454287e0",
+    "url": "/chasebenston/index.html"
+  },
+  {
+    "revision": "011739b974b789048c6c",
+    "url": "/chasebenston/static/css/main.5b11c785.chunk.css"
+  },
+  {
+    "revision": "c9eb13f97bbebfcd4724",
+    "url": "/chasebenston/static/js/2.6008bdc6.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/chasebenston/static/js/2.6008bdc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011739b974b789048c6c",
+    "url": "/chasebenston/static/js/main.88f84351.chunk.js"
+  },
+  {
+    "revision": "853a0b32f9ce155902b8",
+    "url": "/chasebenston/static/js/runtime-main.083b4960.js"
+  }
+]);
