305304035ff1a8bc4d29ba986e5c0bcbfbcb4609
Chase Benston <cbenston@ius.edu>
2020-10-26 15:07:18 -0400

    Updates

--- precache-manifest.20d8b78d611bd208e5aabafac3105f28.js
+++ precache-manifest.20d8b78d611bd208e5aabafac3105f28.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8e517428d843da6bfefeccd458241c4a",
+    "revision": "ff9f54b88b0567cadceeeeb33fd78f98",
     "url": "/chasebenston/index.html"
   },
   {
-    "revision": "ffcbe46e8e8b33aa3ff7",
+    "revision": "15b8dea087a658b9a0e6",
     "url": "/chasebenston/static/css/main.5b11c785.chunk.css"
   },
   {
-    "revision": "8f5ddff6e554a6f69d15",
-    "url": "/chasebenston/static/js/2.493503bd.chunk.js"
+    "revision": "506a3722e68871243b70",
+    "url": "/chasebenston/static/js/2.9a296bf1.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "/chasebenston/static/js/2.493503bd.chunk.js.LICENSE.txt"
+    "url": "/chasebenston/static/js/2.9a296bf1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ffcbe46e8e8b33aa3ff7",
-    "url": "/chasebenston/static/js/main.de462500.chunk.js"
+    "revision": "15b8dea087a658b9a0e6",
+    "url": "/chasebenston/static/js/main.eb508e67.chunk.js"
   },
   {
     "revision": "853a0b32f9ce155902b8",
