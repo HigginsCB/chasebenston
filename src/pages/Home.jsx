bd42ebcb82265052821cae4bf8547ccc08993a79
higginscb <thehigginshut@gmail.com>
2021-04-09 15:18:15 -0400

    Fixed picture link for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
             <Grid container alignItems="center" justify="center" spacing={5} style={{ paddingTop: "25px" }}>
                 <Grid item xs="4">
                     <img
-                        src="https://media-exp1.licdn.com/dms/image/C4D03AQH6ilz88crKWg/profile-displayphoto-shrink_800_800/0?e=1609372800&v=beta&t=0UN09sp8EfiFHkbrXMlwZZ5I828r6XqFsqAScAx6w3c"
+                        src="https://media-exp1.licdn.com/dms/image/C4D03AQH6ilz88crKWg/profile-displayphoto-shrink_800_800/0/1582068283326?e=1623283200&v=beta&t=QIQy_wWBAImC0_doxjrRYyM2PALAXItLpCSzaoLMCDU"
                         style={{width: "100%", paddingTop: "10%"}}
                     />
                 </Grid>
@@ -22,7 +22,8 @@ function Home() {
                         </Typography>
                         <Typography>Since graduation I have been keeping my development skills sharp by completing challenges on various websites (HackerRank, Leetcode, etc.) and collecting <Link to={'/chasebenston/awards'} style={{ color: "inherit" }}>awards</Link> such as badges and skill verifications. 
                             After completing my capstone group <Link to={'/chasebenston/projects'} style={{ color: "inherit" }}>project</Link> using a React and .NET stack I have started development on a personal vlog/blog site for my hobbies using a MERN stack. 
-                            In the meantime I have also been freelancing for my fiance's company (Vividly Bold) to consult with clients on their wishes for WordPress site developments.
+                            In Februrary of 2021 I started with General Motors as a software developer with the online retail team using a Angular and .NET stack.
+                            I occasionally help with my fiance's company (Vividly Bold) by consulting with clients on their wishes for WordPress site developments.
                         </Typography>
                         <Typography>Outside of software engineering I have expanded my skills and hobbies during the social distancing of the pandemic. 
                             I have edited videos (using Premiere Pro) from my trip around the world (The Mongol Rally 2018) and have begun working on some videos around games and other hobbies that I have been expanding during this time. 
--- src/pages/WorkExp.jsx
+++ src/pages/WorkExp.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
 
 
 // Build a component for each work exp and send in information
 
 function WorkExp() {
     return (
-        <div>
-            <h1>Current</h1>
+        <Grid container direction="column">
+            <Typography component="h1">Current</Typography>
+                <h3>General Motors - Software Developer</h3>
+                <h5>Started - Feb 2021</h5>
+                <p></p>
+                <h3>Vividly Bold - Web Developer</h3>
+                <h5>Started - May 2020</h5>
+                <p>Consult with clients for web needs and assist with WordPress development.</p>
             <h1>Past</h1>
-        </div>
+        </Grid>
     );
 }
 
